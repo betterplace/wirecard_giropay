e0f4b9cf21b7933689b31a7421d4376791618250
Gregory Igelmund <gregory.igelmund@gmail.com>
2014-11-21 17:26:16 +0100

    Adds accessors

--- lib/wirecard_giropay/gateway.rb
+++ lib/wirecard_giropay/gateway.rb
@@ -1,5 +1,7 @@
 module WirecardGiropay
   class Gateway
+    attr_reader :business_case_signature, :username, :password
+
     def initialize(opts = {})
       @business_case_signature = opts.fetch(:business_case_signature)
       @username = opts.fetch(:username)
@@ -7,7 +9,7 @@ module WirecardGiropay
     end
 
     def online_wire(params)
-      request  = Request.new params.merge(business_case_signature: @business_case_signature)
+      request  = Request.new params.merge(business_case_signature: business_case_signature)
       response = Typhoeus.post(WirecardGiropay.gateway_url, body: request.to_xml, userpwd: http_auth_credentials)
       Response.from_xml response.body
     end
@@ -15,7 +17,7 @@ module WirecardGiropay
     private
 
     def http_auth_credentials
-      WirecardGiropay.sandboxed? ? WirecardGiropay.sandbox_credentials : [@username, @password].join(':')
+      WirecardGiropay.sandboxed? ? WirecardGiropay.sandbox_credentials : [username, password].join(':')
     end
   end
 end
