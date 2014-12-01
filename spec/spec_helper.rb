7999bcfc90955c43b7ae54c85e99ed2a9c932ac5
Gregory Igelmund <gregory.igelmund@gmail.com>
2014-12-01 16:50:51 +0100

    Removes copy-pasta code

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -9,15 +9,3 @@ end
 SimpleCov.start 'gem'
 
 require 'wirecard_giropay'
-
-RSpec.configure do |config|
-  # config.before do
-  #   Bancard.test = true
-  # end
-  #
-  # config.before(:all) do
-  #   body = { status: :success, process_id: 'lx6niQel0QzWK1g' }
-  #   response = Typhoeus::Response.new(code: 200, body: body.to_json, headers: { 'Location' => 'payment-url' })
-  #   Typhoeus.stub(/vpos/).and_return(response)
-  # end
-end
